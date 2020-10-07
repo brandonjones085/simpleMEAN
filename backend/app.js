e76a64fbea7960b5cf92713ecb4b2754e827655f
brandonjones085 <brandon.jones085@gmail.com>
2020-10-07 12:26:23 -0400

    first

---
+++ backend/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require("./models/post");
+
+const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://brandon085:Bj959211@postdb.kn0jt.mongodb.net/postdb?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.post("/posts", (req, res, next) => {
+  const post = new Post({
+   
+    content: req.body.content
+  });
+  post.save().then(createdPost => {
+    res.status(201).json({
+      message: "Post added successfully",
+      postId: createdPost._id
+    });
+  });
+});
+
+app.get("/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    });
+  });
+});
+
+
+
+module.exports = app;
---
+++ frontend/src/app/posts/posts.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Subject } from "rxjs";
+import { map } from 'rxjs/operators';
+
+import { Post } from "./post.model";
+
+@Injectable({ providedIn: "root" })
+export class PostsService {
+  private posts: Post[] = [];
+  private postsUpdated = new Subject<Post[]>();
+
+  constructor(private http: HttpClient) {}
+
+  getPosts() {
+    this.http
+      .get<{ message: string; posts: any }>(
+        "http://localhost:3000/posts"
+      )
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+          
+            content: post.content,
+            id: post._id
+          };
+        });
+      }))
+      .subscribe(transformedPosts => {
+        this.posts = transformedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  getPostUpdateListener() {
+    return this.postsUpdated.asObservable();
+  }
+
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, content: content };
+    this.http
+      .post<{ message: string, postId: string }>("http://localhost:3000/posts", post)
+      .subscribe(responseData => {
+        const id = responseData.postId;
+        post.id = id;
+        this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+
+}
